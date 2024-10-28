1b1726fefe2ac7b7c915b144947cad2cc5d767d5
Cayman Freeman <freemancayman@gmail.com>
2024-10-27 20:26:17 -0500

    Workflow

--- app.py
+++ app.py
@@ -30,26 +30,26 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(2, weight=1)
         self.grid_rowconfigure(3, weight=1)
 
-        appearance_frame = AppearanceFrame(master=self)
-        appearance_frame.grid(row=0, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.appearance_frame = AppearanceFrame(master=self)
+        self.appearance_frame.grid(row=0, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        click_length_frame = ClickLengthFrame(master=self)
-        click_length_frame.grid(row=1, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.click_length_frame = ClickLengthFrame(master=self)
+        self.click_length_frame.grid(row=1, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        interval_frame = ClickIntervalFrame(master=self)
-        interval_frame.grid(row=2, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.interval_frame = ClickIntervalFrame(master=self)
+        self.interval_frame.grid(row=2, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        click_style_frame = ClickStyleFrame(master=self)
-        click_style_frame.grid(row=3, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.click_style_frame = ClickStyleFrame(master=self)
+        self.click_style_frame.grid(row=3, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        location_frame = LocationFrame(master=self)
-        location_frame.grid(row=4, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.location_frame = LocationFrame(master=self)
+        self.location_frame.grid(row=4, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        start_stop_frame = StartStopFrame(master=self)
-        start_stop_frame.grid(row=6, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.start_stop_frame = StartStopFrame(master=self)
+        self.start_stop_frame.grid(row=6, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
-        hotkey_frame = HotkeyFrame(master=self, start_stop_frame=start_stop_frame)
-        hotkey_frame.grid(row=5, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
+        self.hotkey_frame = HotkeyFrame(master=self, start_stop_frame=self.start_stop_frame)
+        self.hotkey_frame.grid(row=5, column=0, padx=FRAME_PADDING, pady=FRAME_PADDING, sticky="ew")
 
         self.update()
 
