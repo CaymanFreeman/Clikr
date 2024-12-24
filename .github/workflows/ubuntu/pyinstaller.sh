e69543b211091fe126cfefc5755f65b653ece3db
Cayman Freeman <violetdepth.humvee479@passinbox.com>
2024-12-24 17:34:36 -0600

    Workflow Simplification

--- .github/workflows/ubuntu/pyinstaller.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pyinstaller --noconfirm --onefile --name "easyautoclicker" --add-data="LICENSE.md:." --hidden-import pynput.keyboard._xorg --hidden-import pynput.mouse._xorg src/main.py
--- .github/workflows/windows/pyinstaller.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-pyinstaller --noconfirm --onefile --name "EasyAutoClicker" --windowed --add-data="LICENSE.md;." --icon="assets\icon.ico" --add-data="assets\icon.png;assets" src\main.py
